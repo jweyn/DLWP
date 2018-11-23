02a1100965427bd2521052a28e3f2ddf77966be7
jweyn <jweyn@uw.edu>
2018-11-23 10:35:00 -0800

    Updated training example

--- train_dlwp.py
+++ train_dlwp.py
@@ -14,34 +14,38 @@ from DLWP.model import DLWPNeuralNet, DataGenerator, Preprocessor
 from DLWP.model.preprocessing import train_test_split_ind
 from DLWP.util import RNNResetStates, save_model
 from keras.regularizers import l2
-from keras.callbacks import History
+from keras.callbacks import History, EarlyStopping
 
 
 #%% Open some data using the Preprocessor wrapper
 
 root_directory = '/home/disk/wave2/jweyn/Data/DLWP'
-predictor_file = '%s/cfs_2000-2009_hgt_500_NH.nc' % root_directory
-model_file = '%s/dlwp_2000-2009_hgt_500_NH_CLSTM_16_5_2' % root_directory
-epochs = 50
+predictor_file = '%s/cfs_2000-2009_hgt_300-700_NH.nc' % root_directory
+model_file = '%s/dlwp_2000-2009_hgt_300-700_NH_CONV_16_5_2' % root_directory
+epochs = 100
+batch_size = 216
 lambda_ = 1.e-3
 
 processor = Preprocessor(None, predictor_file=predictor_file)
 processor.open()
 
 n_sample = processor.data.dims['sample']
-n_val = int(np.floor(n_sample * 0.2))
+# If validation set must be a multiple of batch_size (i.e., stateful RNN):
+n_val = int(np.floor(n_sample * 0.2)) // batch_size * batch_size
+# Fixed last 2 years
+n_val = 4 * (365 + 366)
 train_set, val_set = train_test_split_ind(n_sample, n_val, method='last')
 
 
-#%%  Build a model and the data generators
+#%% Build a model and the data generators
 
 dlwp = DLWPNeuralNet(scaler_type=None, scale_targets=False)
 
 # Build the data generators
-generator = DataGenerator(dlwp, processor.data.isel(sample=train_set), batch_size=216,
-                          convolution=True, add_time_axis=True)
-val_generator = DataGenerator(dlwp, processor.data.isel(sample=val_set), batch_size=216,
-                              convolution=True, add_time_axis=True)
+generator = DataGenerator(dlwp, processor.data.isel(sample=train_set), batch_size=batch_size,
+                          convolution=True, add_time_axis=False)
+val_generator = DataGenerator(dlwp, processor.data.isel(sample=val_set), batch_size=batch_size,
+                              convolution=True, add_time_axis=False)
 
 
 #%% Compile the model structure with some generator data information
@@ -59,40 +63,46 @@ val_generator = DataGenerator(dlwp, processor.data.isel(sample=val_set), batch_s
 #     ('Reshape', ((1, generator.n_features),), None)
 # )
 
-# Convolutional LSTM (set add_time_axis=True and convolution=True for generators)
+# # Convolutional LSTM (set add_time_axis=True and convolution=True for generators)
+# layers = (
+#     ('ConvLSTM2D', (16, 5), {
+#         'strides': 2,
+#         'activation': 'tanh',
+#         'kernel_regularizer': l2(lambda_),
+#         'return_sequences': True,
+#         # 'stateful': True,
+#         'data_format': 'channels_first',
+#         # 'batch_input_shape': (batch_size, 1,) + generator.convolution_shape,
+#         'input_shape': (1,) + generator.convolution_shape
+#     }),
+#     ('Flatten', None, None),
+#     ('Dense', (generator.n_features,), {'activation': 'linear'}),
+#     ('Reshape', ((1,) + generator.convolution_shape,), None)
+# )
+
+# Convolutional feed-forward (set convolution=True for generators)
 layers = (
-    ('ConvLSTM2D', (16, 5), {
+    ('Conv2D', (16, 5), {
         'strides': 2,
         'activation': 'tanh',
         'kernel_regularizer': l2(lambda_),
-        'return_sequences': True,
         'data_format': 'channels_first',
-        'input_shape': (1,) + generator.convolution_shape
+        'input_shape': generator.convolution_shape
     }),
     ('Flatten', None, None),
-    ('Dense', (generator.n_features,), None),
-    ('Reshape', ((1,) + generator.convolution_shape,), None)
+    ('Dense', (generator.n_features,), {'activation': 'linear'}),
+    ('Reshape', (generator.convolution_shape,), None)
 )
 
 # # Feed-forward dense neural network
 # layers = (
-#     ('Dense', (1024,), {
+#     ('Dense', (generator.n_features // 2,), {
 #         'activation': 'tanh',
-#         # 'kernel_regularizer': l2(lambda_),
+#         'kernel_regularizer': l2(lambda_),
 #         'input_shape': (generator.n_features,)
 #     }),
-#     ('Dense', (1024,), {
-#         'activation': 'tanh',
-#         # 'kernel_regularizer': l2(lambda_)
-#     }),
-#     ('Dense', (1024,), {
-#         'activation': 'tanh',
-#         # 'kernel_regularizer': l2(lambda_)
-#     }),
-#     ('Dense', (generator.n_features,), {
-#         'activation': 'linear',
-#         # 'kernel_regularizer': l2(lambda_)
-#     }),
+#     ('Dropout', (0.25,), None),
+#     ('Dense', (generator.n_features,), {'activation': 'linear'})
 # )
 
 dlwp.build_model(layers, loss='mse', optimizer='adam', metrics=['mae'])
@@ -108,7 +118,8 @@ p_fit, t_fit = generator.generate(fit_set, scale_and_impute=False)
 dlwp.init_fit(p_fit, t_fit)
 p_fit, t_fit = (None, None)
 
-# If system memory permits, loading the predictor data greatly increases efficiency when training on GPUs.
+# If system memory permits, loading the predictor data can greatly increases efficiency when training on GPUs, if the
+# train computation takes less time than the data loading.
 # generator.ds.load()
 
 # Load the validation data. Better to load in memory to avoid file read errors while training.
@@ -120,8 +131,9 @@ p_val, t_val = val_generator.generate([], scale_and_impute=False)
 # Train and evaluate the model
 start_time = time.time()
 history = History()
-dlwp.fit_generator(generator, epochs=epochs, verbose=1, validation_data=(p_val, t_val),
-                   use_multiprocessing=True, callbacks=[history, RNNResetStates()])
+early = EarlyStopping(monitor='val_mean_absolute_error', min_delta=1.e-3, patience=15, restore_best_weights=True)
+dlwp.fit_generator(generator, epochs=epochs, verbose=1, validation_data=(p_val, t_val), use_multiprocessing=True,
+                   callbacks=[history, RNNResetStates(), early], shuffle=False)
 end_time = time.time()
 
 # Evaluate the model
